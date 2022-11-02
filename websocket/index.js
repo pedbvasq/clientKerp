cacdfa9153abbfa3c51e0c89495ff1e83e0d65fd
pedbvasq <pedbva@espol.edu.ec>
2022-11-02 16:22:34 -0500

    conexion en tiempo real de backend y front

--- websocket/index.js
+++ websocket/index.js
@@ -5,23 +5,17 @@ const server = require("http").createServer(app);
 const WebSocket = require("ws");
 const wss = new WebSocket.Server({ server: server });
 const fs = require("fs");
-const { send } = require("process");
-
+var clients = [];
 app.use(express.static(path.join(__dirname, "./public")));
 
 wss.on("connection", function connection(ws) {
-  console.log("new cliente connect");
-  for (var i = 0; i < 9; i++) {
-    var json = {
-      nombre: "Pablooo vasquez",
-      cedulaRuc: "12345456" + i,
-      correo: "pepito@hotmail.com",
-      direccion: "mz l3 villa 4",
-    };
-    ws.send(JSON.stringify(json));
-  }
+  clients.push(ws);
 
-  ws.on("message", function incoming(message) {});
+  ws.on("message", function incoming(message) {
+    clients.forEach(function (client) {
+      client.send(String(message));
+    });
+  });
 });
 
 app.get("/");
